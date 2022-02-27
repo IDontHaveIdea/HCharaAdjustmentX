957f80f0c36814cf9f0bc2a24ef759e29230dcbe
IDontHaveIdea <88360628+IDontHaveIdea@users.noreply.github.com>
2022-02-27 02:46:41 -0500

    Cleanup

--- HCharacterAdjustmentX.KK/GlobalSuppressions.cs
+++ HCharacterAdjustmentX.KK/GlobalSuppressions.cs
@@ -6,10 +6,9 @@
 using System.Diagnostics.CodeAnalysis;
 
 [assembly: SuppressMessage("Style", "IDE0022:Use block body for methods", Justification = "<Pending>", Scope = "member", Target = "~M:IDHIPlugins.HProcScene.InvokeOnHSceneStartLoading(System.Object,System.EventArgs)")]
-[assembly: SuppressMessage("Style", "IDE0044:Add readonly modifier", Justification = "<Pending>", Scope = "member", Target = "~F:IDHIPlugins.HCharaterAdjustX.ButtonsGUI2._buttons")]
-[assembly: SuppressMessage("Style", "IDE0044:Add readonly modifier", Justification = "<Pending>", Scope = "member", Target = "~F:IDHIPlugins.HCharaterAdjustX.ButtonsGUI._buttons")]
-[assembly: SuppressMessage("Style", "IDE0022:Use block body for methods", Justification = "<Pending>", Scope = "member", Target = "~M:IDHIPlugins.HCharaterAdjustX.HCharacterAdjustXController.SetOriginalPosition")]
-[assembly: SuppressMessage("Style", "IDE0022:Use block body for methods", Justification = "<Pending>", Scope = "member", Target = "~M:IDHIPlugins.HCharaterAdjustX.HCharacterAdjustXController.InvokeOnMoveRequest(System.Object,IDHIPlugins.HCharaterAdjustX.HCharacterAdjustXController.MoveRequestEventArgs)")]
-[assembly: SuppressMessage("Style", "IDE0022:Use block body for methods", Justification = "<Pending>", Scope = "member", Target = "~M:IDHIPlugins.HCharaterAdjustX.GetController(ChaControl)~IDHIPlugins.HCharaterAdjustX.HCharacterAdjustXController")]
+[assembly: SuppressMessage("Style", "IDE0044:Add readonly modifier", Justification = "<Pending>", Scope = "member", Target = "~F:IDHIPlugins.HCharaAdjustmentX.ButtonsGUI2._buttons")]
+[assembly: SuppressMessage("Style", "IDE0044:Add readonly modifier", Justification = "<Pending>", Scope = "member", Target = "~F:IDHIPlugins.HCharaAdjustmentX.ButtonsGUI._buttons")]
+[assembly: SuppressMessage("Style", "IDE0022:Use block body for methods", Justification = "<Pending>", Scope = "member", Target = "~M:IDHIPlugins.HCharaAdjustmentX.HCharaAdjusmentXController.SetOriginalPosition")]
 [assembly: SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "Needed to enable the patch", Scope = "member", Target = "~M:IDHIPlugins.Utils.SceneChanged(UnityEngine.SceneManagement.Scene,UnityEngine.SceneManagement.Scene)")]
-[assembly: SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "<Pending>", Scope = "member", Target = "~M:IDHIPlugins.HCharaterAdjustX.Hooks.ChangeCategoryPostfix(H.HPointData,System.Int32)")]
+[assembly: SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "<Pending>", Scope = "member", Target = "~M:IDHIPlugins.HCharaAdjustmentX.Hooks.ChangeCategoryPostfix(H.HPointData,System.Int32)")]
+[assembly: SuppressMessage("Style", "IDE0022:Use block body for methods", Justification = "<Pending>", Scope = "member", Target = "~M:IDHIPlugins.HCharaAdjustmentX.GetController(ChaControl)~HCharacterAdjustX.HCharacterAdjustXPlugin.HCharacterAdjustXController")]
