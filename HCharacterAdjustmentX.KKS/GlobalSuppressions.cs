5c68220e2cb42ec5b03536e772a82c16e058c9a2
IDontHaveIdea <88360628+IDontHaveIdea@users.noreply.github.com>
2022-02-27 02:47:17 -0500

    Refactoring

--- HCharacterAdjustmentX.KKS/GlobalSuppressions.cs
+++ HCharacterAdjustmentX.KKS/GlobalSuppressions.cs
@@ -5,9 +5,27 @@
 
 using System.Diagnostics.CodeAnalysis;
 
-[assembly: SuppressMessage("Style", "IDE0022:Use block body for methods", Justification = "<Pending>", Scope = "member", Target = "~M:IDHIPlugins.HProcScene.InvokeOnHSceneStartLoading(System.Object,System.EventArgs)")]
-[assembly: SuppressMessage("Style", "IDE0044:Add readonly modifier", Justification = "<Pending>", Scope = "member", Target = "~F:IDHIPlugins.HCharaterAdjustX.ButtonsGUI2._buttons")]
-[assembly: SuppressMessage("Style", "IDE0044:Add readonly modifier", Justification = "<Pending>", Scope = "member", Target = "~F:IDHIPlugins.HCharaterAdjustX.ButtonsGUI._buttons")]
-[assembly: SuppressMessage("Style", "IDE0022:Use block body for methods", Justification = "<Pending>", Scope = "member", Target = "~M:IDHIPlugins.HCharaterAdjustX.HCharacterAdjustXController.SetOriginalPosition")]
-[assembly: SuppressMessage("Style", "IDE0022:Use block body for methods", Justification = "<Pending>", Scope = "member", Target = "~M:IDHIPlugins.HCharaterAdjustX.HCharacterAdjustXController.InvokeOnMoveRequest(System.Object,IDHIPlugins.HCharaterAdjustX.HCharacterAdjustXController.MoveRequestEventArgs)")]
-[assembly: SuppressMessage("Style", "IDE0022:Use block body for methods", Justification = "<Pending>", Scope = "member", Target = "~M:IDHIPlugins.HCharaterAdjustX.GetController(ChaControl)~IDHIPlugins.HCharaterAdjustX.HCharacterAdjustXController")]
+[assembly: SuppressMessage(
+    "Style",
+    "IDE0044:Add readonly modifier",
+    Justification = "Cannot update _buttons in OnGui otherwise",
+    Scope = "member",
+    Target = "~F:IDHIPlugins.HCharaAdjustmentX.ButtonsGUI2._buttons")]
+[assembly: SuppressMessage(
+    "Style",
+    "IDE0044:Add readonly modifier",
+    Justification = "Cannot update _buttons in OnGui otherwise",
+    Scope = "member",
+    Target = "~F:IDHIPlugins.HCharaAdjustmentX.ButtonsGUI._buttons")]
+[assembly: SuppressMessage(
+    "Style",
+    "IDE0060:Remove unused parameter",
+    Justification = "Needed for patch to work",
+    Scope = "member",
+    Target = "~M:IDHIPlugins.HCharaAdjustmentX.Hooks.ChangeCategoryPostfix(H.HPointData,System.Int32)")]
+[assembly: SuppressMessage(
+    "Style",
+    "IDE0060:Remove unused parameter",
+    Justification = "Part of API",
+    Scope = "member",
+    Target = "~M:IDHIPlugins.Utils.SceneChanged(UnityEngine.SceneManagement.Scene,UnityEngine.SceneManagement.Scene)")]
