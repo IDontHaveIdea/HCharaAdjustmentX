11a3e408f041020bb3efbbbf77ebcf74e8aef9ad
IDontHaveIdea <88360628+IDontHaveIdea@users.noreply.github.com>
2022-02-27 02:46:26 -0500

    Add project guid

---
+++ HCharacterAdjustmentX.KK/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+using System.Runtime.InteropServices;
+
+[assembly: Guid("6FF4B0F9-EE1D-4CCF-8432-E355E54F667D")]
