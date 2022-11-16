78ed6ee0c1a3715655cdcb63ffb4621e72986c5a
IDontHaveIdea <88360628+IDontHaveIdea@users.noreply.github.com>
2022-11-16 15:52:42 -0500

    Add Extension file

---
+++ HCharacterAdjustmentX.Core/Utils/Extensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using HarmonyLib;
+
+using CTRL = IDHIPlugins.HCharaAdjustmentX.HCharaAdjusmentXController;
+namespace IDHIPlugins
+{
+    internal static class Extensions
+    {
+        public static CTRL GetController(this ChaControl chaControl)
+        {
+            return chaControl == null
+                ? null : chaControl.GetComponent<CTRL>();
+        }
+
+        public static CTRL GetController(this SaveData.Heroine heroine)
+        {
+            return GetController(heroine?.chaCtrl);
+        }
+    }
+}
