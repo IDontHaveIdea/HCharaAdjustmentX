4f35dfdb01ce5c779f60b49d41b0cf1e30a05345
IDontHaveIdea <88360628+IDontHaveIdea@users.noreply.github.com>
2023-02-28 09:38:10 -0500

    Refactoring

--- HCharacterAdjustmentX.Core/Buttons/Properties.cs
+++ HCharacterAdjustmentX.Core/Buttons/Properties.cs
@@ -1,11 +1,21 @@
 ﻿//
-// MoveEvent
+// Buttons Properties
 //
 using System.Collections.Generic;
+
+using UnityEngine;
 
 
 namespace IDHIPlugins
 {
+    public enum Axis
+    {
+        UNKNOWN = -1,
+        X = 0,
+        Y = 1,
+        Z = 2
+    }
+
     public enum ActionType
     {
         POSITION,
@@ -47,8 +57,29 @@ namespace IDHIPlugins
             { new(   "Axis", MoveType.AXIS, ActionType.AXIS, true) },
             { new(   "Save", MoveType.SAVE, ActionType.POSITION) },
             { new(   "Load", MoveType.LOAD, ActionType.POSITION) },
-            { new("R. Move", MoveType.RESETMOVE, ActionType.POSITION) },
+            { new("R. Move", MoveType.RESETPOSITION, ActionType.POSITION) },
             { new("R. Rot.", MoveType.RESETROTATION, ActionType.ROTATION) }
         };
+    }
+
+    public class AxisVector
+    {
+        private static readonly Dictionary<Axis, Vector3> _values = new()
+        {
+            {Axis.X, Vector3.right},
+            {Axis.Y, Vector3.up},
+            {Axis.Z, Vector3.forward}
+        };
+
+        public static bool TryGetValue(Axis key, out Vector3 result)
+        {
+            return _values.TryGetValue(key, out result);
+        }
+
+        public Vector3 this[Axis key]
+        {
+            get { return _values[key]; }
+            set { _values[key] = value; }
+        }
     }
 }
