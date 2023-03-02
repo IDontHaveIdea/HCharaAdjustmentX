e525a1b97556e109e3ba0348d6c40a1db4fe4a94
IDontHaveIdea <88360628+IDontHaveIdea@users.noreply.github.com>
2023-03-02 01:50:48 -0500

    Log messages Debug version only some refactoring

--- HCharacterAdjustmentX.Core/Buttons/AxisButton.cs
+++ HCharacterAdjustmentX.Core/Buttons/AxisButton.cs
@@ -1,4 +1,4 @@
-//
+﻿//
 // ActionButton
 //
 using UnityEngine;
@@ -16,7 +16,7 @@ namespace IDHIPlugins
         private readonly HCharaAdjusmentXController _controller;
 
         #region Interface Properties
-        public string Text => $"Axis: {_currentAxis}";
+        public string Text => $"Axis: {_currentAxis} ({AxisVector.Color[_currentAxis]})";
         public Color BackgroundColor { get; set; }
         public Color ForegroundColor { get; set; }
         public Rect Position { get; }
@@ -34,10 +34,14 @@ namespace IDHIPlugins
             {
                 _currentAxis = value;
                 _controller.CurrentAxis = value;
+#if DEBUG
+                _Log.Warning($"[Axis] _currentAxis={_currentAxis} for ChaType={ChaType} " +
+                    $"set to={_controller.CurrentAxis}");
+#endif
             }
         }
-        public CharacterType ChaType { get; private set; }
-        #endregion
+        public CharacterType ChaType { get; set; }
+#endregion
 
         #region Constructors
         public AxisButton(
@@ -45,19 +49,30 @@ namespace IDHIPlugins
             CharacterType chaType
             )
         {
+            ChaType = chaType;
             _controller = GetControllerByType(chaType);
             Position = position;
             Axis = Axis.Y;
-            ChaType = chaType;
             ForegroundColor = Color.white;
             BackgroundColor = Color.gray;
+#if DEBUG
+            _Log.Warning($"[Axis] Constructor for {chaType} set ChaType={ChaType} " +
+                $"set axis to={_controller.CurrentAxis} controller " +
+                $"type={_controller.ChaType}");
+#endif
         }
-        #endregion
+#endregion
 
         #region Interface public methods
         public void Process()
         {
+#if DEBUG
+            var axis = _controller.CurrentAxis;
+#endif
             Axis = (Axis)(((int)_currentAxis + 1) % 3);
+#if DEBUG
+            _Log.Info($"[Axis.Process] Change axis from {axis} to {Axis}.");
+#endif
         }
         #endregion
     }
