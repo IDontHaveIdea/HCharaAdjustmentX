1a6d95455829042cd8ae2d8e2c1a50538e3bff68
IDontHaveIdea <88360628+IDontHaveIdea@users.noreply.github.com>
2023-06-03 16:14:55 -0400

    Update message suppressions

--- HCharacterAdjustmentX.KK/GlobalSuppressions.cs
+++ HCharacterAdjustmentX.KK/GlobalSuppressions.cs
@@ -9,5 +9,5 @@ using System.Diagnostics.CodeAnalysis;
 [assembly: SuppressMessage("Style", "IDE0044:Add readonly modifier", Justification = "<Pending>", Scope = "member", Target = "~F:IDHIPlugIns.HCharaAdjustmentX.ButtonsGUI2._buttons")]
 [assembly: SuppressMessage("Style", "IDE0044:Add readonly modifier", Justification = "<Pending>", Scope = "member", Target = "~F:IDHIPlugIns.HCharaAdjustmentX.ButtonsGUI._buttons")]
 [assembly: SuppressMessage("Style", "IDE0022:Use block body for methods", Justification = "<Pending>", Scope = "member", Target = "~M:IDHIPlugIns.HCharaAdjustmentX.HCharaAdjustmentXController.SetOriginalPosition")]
-[assembly: SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "Needed to enable the patch", Scope = "member", Target = "~M:IDHIPlugIns.Utils.SceneChanged(UnityEngine.SceneManagement.Scene,UnityEngine.SceneManagement.Scene)")]
+[assembly: SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "Needed to enable the patch", Scope = "member", Target = "~M:IDHIPlugIns.PlugInUtils.SceneChanged(UnityEngine.SceneManagement.Scene,UnityEngine.SceneManagement.Scene)")]
 [assembly: SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "<Pending>", Scope = "member", Target = "~M:IDHIPlugIns.HCharaAdjustmentX.Hooks.ChangeCategoryPostfix(H.HPointData,System.Int32)")]
